f3d839d54c2651ee9b78ae23c542615239533ad7
Elias Hall <elias.hall@gmail.com>
2020-05-19 15:30:10 -0500

    Initial Commit

---
+++ GetCoordinates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetCoordinates
+//
+//  Created by Elias Hall on 5/19/20.
+//  Copyright © 2020 Elias Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
